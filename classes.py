0dfb9cfb912eef2d6967ba5de16baf0fc0f9d63d
Adeyinka Adewojo <adeywojo@gmail.com>
2021-04-22 08:27:54 -0500

    BudgetApp v1

---
+++ classes.py
@@ -0,0 +1,21 @@
+class Category:
+    def __init__(self, first_name, last_name, expense, amount, balance):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.expense = expense
+        self.amount = amount
+        self.balance = balance
+
+    def deposit(self):
+        pass
+
+    def withdraw(self):
+        pass
+
+    def check_balance(self):
+        pass
+
+    def transfer(self):
+        pass
+
+    input("Enter your First name: \n")
